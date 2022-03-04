2263a79696dfce809c31cf8a8685cd683214995a
calvix <vaclav@giantswarm.io>
2022-03-04 09:07:05 +0100

    add-version (#3)

---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "The irsa-operator manages IAM Roles for Service Accounts for aws cluster."
+	gitSHA      = "n/a"
+	name        = "irsa-operator"
+	source      = "https://github.com/giantswarm/irsa-operator"
+	version     = "0.0.1-dev"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
