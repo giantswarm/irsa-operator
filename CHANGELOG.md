843f56dcc21f82da16b05e3f754c5d65d30e6b24
Nick Jüttner <nick@giantswarm.io>
2022-03-14 14:07:07 +0100

    add capa-controller

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add `capa-controller` to reconcile Cluster API Provider AWS CR's.
+
 ## [0.1.1] - 2022-03-09
 
 ### Added
--- main.go
+++ main.go
@@ -81,14 +81,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
 		os.Exit(1)
 	}
-	//if err = (&controllers.CAPAClusterReconciler{
-	//	Client: mgr.GetClient(),
-	//	Log:    ctrl.Log.WithName("capa-controller"),
-	//	Scheme: mgr.GetScheme(),
-	//}).SetupWithManager(mgr); err != nil {
-	//	setupLog.Error(err, "unable to create controller", "controller", "Cluster")
-	//	os.Exit(1)
-	//}
+	if err = (&controllers.CAPAClusterReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("capa-controller"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
