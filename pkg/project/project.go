cd1e4aa6ec4b6da25e10d4ee3ffe44f926aa7e45
GitHub Action <action@github.com>
2022-04-15 08:05:45 +0000

    Bump version to 0.3.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "irsa-operator"
 	source      = "https://github.com/giantswarm/irsa-operator"
-	version     = "0.3.1"
+	version     = "0.3.2-dev"
 )
 
 func Description() string {
