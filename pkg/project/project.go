0683203c3819e7532d4d60a458f8bf0cc7ec163a
Christian Bianchi <christian@giantswarm.io>
2022-11-09 16:28:28 +0100

    Update project.go

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "irsa-operator"
 	source      = "https://github.com/giantswarm/irsa-operator"
-	version     = "0.8.4-dev"
+	version     = "0.8.5-dev"
 )
 
 func Description() string {
