dbab86db3afa082901a54db9f805e81ace71db17
Christian Bianchi <whites11@gmail.com>
2022-11-14 16:32:38 +0100

    improve oidc provider

--- pkg/aws/services/iam/oidc.go
+++ pkg/aws/services/iam/oidc.go
@@ -28,58 +28,93 @@ func (s *Service) EnsureOIDCProviders(identityProviderURLs []string, clientID st
 			return err
 		}
 
-	arn, existing, err := s.findOIDCProvider()
-	if err != nil {
-		return microerror.Mask(err)
-	}
+		// Check if one of the providers is already using the right URL.
+		found := false
+		for arn, existing := range providers {
+			if util.EnsureHTTPS(*existing.Url) == util.EnsureHTTPS(identityProviderURL) {
+				// Check if values are up to date.
+				if len(existing.ThumbprintList) != 1 || !strings.EqualFold(*existing.ThumbprintList[0], strings.ToLower(tp)) ||
+					len(existing.ClientIDList) != 1 || *existing.ClientIDList[0] != clientID {
 
-	if existing != nil {
-		// Check if values are up to date.
-		if fmt.Sprintf("https://%s", *existing.Url) != identityProviderURL ||
-			len(existing.ThumbprintList) != 1 || !strings.EqualFold(*existing.ThumbprintList[0], strings.ToLower(tp)) ||
-			len(existing.ClientIDList) != 1 || *existing.ClientIDList[0] != clientID {
-
-			s.scope.Info("OIDCProvider needs to be replaced")
-			s.scope.Info("Deleting old OIDCProvider")
-			_, err = s.Client.DeleteOpenIDConnectProvider(&iam.DeleteOpenIDConnectProviderInput{OpenIDConnectProviderArn: aws.String(arn)})
-			if err != nil {
-				return microerror.Mask(err)
+					s.scope.Info(fmt.Sprintf("OIDCProvider for URL %s needs to be replaced", identityProviderURL))
+					s.scope.Info("Deleting OIDCProvider")
+					_, err = s.Client.DeleteOpenIDConnectProvider(&iam.DeleteOpenIDConnectProviderInput{OpenIDConnectProviderArn: aws.String(arn)})
+					if err != nil {
+						return microerror.Mask(err)
+					}
+					s.scope.Info("Deleted OIDCProvider")
+				} else {
+					found = true
+					break
+				}
 			}
-			s.scope.Info("Deleted old OIDCProvider")
-		} else {
-			s.scope.Info("OIDCProvider already exists and is up to date")
-			return nil
 		}
-	}
 
-	i := &iam.CreateOpenIDConnectProviderInput{
-		Url:            aws.String(identityProviderURL),
-		ThumbprintList: []*string{aws.String(tp)},
-		ClientIDList:   []*string{aws.String(clientID)},
-	}
+		if found {
+			s.scope.Info(fmt.Sprintf("OIDCProvider for URL %s already exists and is up to date", identityProviderURL))
+			continue
+		}
 
-	_, err = s.Client.CreateOpenIDConnectProvider(i)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-	s.scope.Info("Created OIDC provider")
+		s.scope.Info(fmt.Sprintf("Creating OIDCProvider for URL %s", identityProviderURL))
 
+		i := &iam.CreateOpenIDConnectProviderInput{
+			Url:            aws.String(identityProviderURL),
+			ThumbprintList: []*string{aws.String(tp)},
+			ClientIDList:   []*string{aws.String(clientID)},
+		}
+
+		// Add internal and customer tags.
+		{
+			for k, v := range s.internalTags() {
+				tag := &iam.Tag{
+					Key:   aws.String(k),
+					Value: aws.String(v),
+				}
+				i.Tags = append(i.Tags, tag)
+			}
+
+			for k, v := range customerTags {
+				tag := &iam.Tag{
+					Key:   aws.String(k),
+					Value: aws.String(v),
+				}
+				i.Tags = append(i.Tags, tag)
+			}
+		}
+
+		_, err = s.Client.CreateOpenIDConnectProvider(i)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		s.scope.Info(fmt.Sprintf("Created OIDC provider for URL %s", identityProviderURL))
+	}
 	return nil
 }
 
-func (s *Service) findOIDCProvider() (string, *iam.GetOpenIDConnectProviderOutput, error) {
-	s.scope.Info("Looking for existing OIDC provider")
+func (s *Service) internalTags() map[string]string {
+	return map[string]string{
+		key.S3TagOrganization: util.RemoveOrg(s.scope.ClusterNamespace()),
+		key.S3TagCluster:      s.scope.ClusterName(),
+		fmt.Sprintf(key.S3TagCloudProvider, s.scope.ClusterName()): "owned",
+		key.S3TagInstallation: s.scope.Installation(),
+	}
+}
+
+func (s *Service) findOIDCProviders() (map[string]*iam.GetOpenIDConnectProviderOutput, error) {
+	s.scope.Info("Looking for existing OIDC providers")
 	output, err := s.Client.ListOpenIDConnectProviders(&iam.ListOpenIDConnectProvidersInput{})
 	if err != nil {
-		return "", nil, microerror.Mask(err)
+		return nil, microerror.Mask(err)
 	}
+
+	ret := make(map[string]*iam.GetOpenIDConnectProviderOutput, 0)
 
 	for _, providerArn := range output.OpenIDConnectProviderList {
 		p, err := s.Client.GetOpenIDConnectProvider(&iam.GetOpenIDConnectProviderInput{
 			OpenIDConnectProviderArn: providerArn.Arn,
 		})
 		if err != nil {
-			return "", nil, microerror.Mask(err)
+			return nil, microerror.Mask(err)
 		}
 
 		// Check if tags match
@@ -95,55 +130,17 @@ func (s *Service) findOIDCProvider() (string, *iam.GetOpenIDConnectProviderOutpu
 		}
 
 		if installationTagFound && clusterTagFound {
-			s.scope.Info("Found existing OIDC provider")
-			return *providerArn.Arn, p, nil
+			ret[*providerArn.Arn] = p
 		}
 	}
 
-	s.scope.Info("Did not find any OIDC provider")
-
-	return "", nil, nil
-}
-
-func (s *Service) CreateOIDCTags(release *semver.Version, cfDomain, accountID, bucketName, region string, customerTags map[string]string) error {
-	var providerArn string
-	if (key.IsV18Release(release) && !key.IsChina(region)) || (s.scope.MigrationNeeded() && !key.IsChina(region)) {
-		providerArn = fmt.Sprintf("arn:%s:iam::%s:oidc-provider/%s", key.ARNPrefix(region), accountID, cfDomain)
+	if len(ret) == 0 {
+		s.scope.Info("Did not find any OIDC provider")
 	} else {
-		providerArn = fmt.Sprintf("arn:%s:iam::%s:oidc-provider/s3.%s.%s/%s", key.ARNPrefix(region), accountID, region, key.AWSEndpoint(region), bucketName)
-	}
-	i := &iam.TagOpenIDConnectProviderInput{
-		OpenIDConnectProviderArn: aws.String(providerArn),
-		Tags: []*iam.Tag{
-			{
-				Key:   aws.String(key.S3TagOrganization),
-				Value: aws.String(util.RemoveOrg(s.scope.ClusterNamespace())),
-			},
-			{
-				Key:   aws.String(key.S3TagCluster),
-				Value: aws.String(s.scope.ClusterName()),
-			},
-			{
-				Key:   aws.String(fmt.Sprintf(key.S3TagCloudProvider, s.scope.ClusterName())),
-				Value: aws.String("owned"),
-			},
-			{
-				Key:   aws.String(key.S3TagInstallation),
-				Value: aws.String(s.scope.Installation()),
-			},
-		},
+		s.scope.Info(fmt.Sprintf("Found %d existing OIDC providers", len(ret)))
 	}
 
-	for k, v := range customerTags {
-		i.Tags = append(i.Tags, &iam.Tag{Key: aws.String(k), Value: aws.String(v)})
-	}
-
-	_, err := s.Client.TagOpenIDConnectProvider(i)
-	if err != nil {
-		return err
-	}
-	s.scope.Info("Created tags for OIDC provider")
-	return nil
+	return ret, nil
 }
 
 func (s *Service) ListCustomerOIDCTags(release *semver.Version, cfDomain, accountID, bucketName, region string) (map[string]string, error) {
@@ -173,53 +170,30 @@ func (s *Service) ListCustomerOIDCTags(release *semver.Version, cfDomain, accoun
 	return oidcTags, nil
 }
 
-func (s *Service) RemoveOIDCTags(release *semver.Version, cfDomain, accountID, bucketName, region string, tagKeys []string) error {
-	var providerArn string
-	if (key.IsV18Release(release) && !key.IsChina(region)) || (s.scope.MigrationNeeded() && !key.IsChina(region)) {
-		providerArn = fmt.Sprintf("arn:%s:iam::%s:oidc-provider/%s", key.ARNPrefix(region), accountID, cfDomain)
-	} else {
-		providerArn = fmt.Sprintf("arn:%s:iam::%s:oidc-provider/s3.%s.%s/%s", key.ARNPrefix(region), accountID, region, key.AWSEndpoint(region), bucketName)
-	}
-	i := &iam.UntagOpenIDConnectProviderInput{
-		OpenIDConnectProviderArn: aws.String(providerArn),
-		TagKeys:                  []*string{},
-	}
-
-	for _, t := range tagKeys {
-		i.TagKeys = append(i.TagKeys, aws.String(t))
-	}
-
-	_, err := s.Client.UntagOpenIDConnectProvider(i)
+func (s *Service) DeleteOIDCProviders() error {
+	providers, err := s.findOIDCProviders()
 	if err != nil {
-		return err
-	}
-	s.scope.Info("Removed tags for OIDC provider")
-	return nil
-}
-
-func (s *Service) DeleteOIDCProvider(release *semver.Version, cfDomain, accountID, bucketName, region string) error {
-	var providerArn string
-	if (key.IsV18Release(release) && !key.IsChina(region)) || (s.scope.MigrationNeeded() && !key.IsChina(region)) {
-		providerArn = fmt.Sprintf("arn:%s:iam::%s:oidc-provider/%s", key.ARNPrefix(region), accountID, cfDomain)
-	} else {
-		providerArn = fmt.Sprintf("arn:%s:iam::%s:oidc-provider/s3.%s.%s/%s", key.ARNPrefix(region), accountID, region, key.AWSEndpoint(region), bucketName)
-	}
-	i := &iam.DeleteOpenIDConnectProviderInput{
-		OpenIDConnectProviderArn: aws.String(providerArn),
+		return microerror.Mask(err)
 	}
 
-	_, err := s.Client.DeleteOpenIDConnectProvider(i)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case iam.ErrCodeNoSuchEntityException:
-				s.scope.Info("OIDC provider no longer exists, skipping deletion")
-				return nil
-			}
+	for providerArn := range providers {
+		i := &iam.DeleteOpenIDConnectProviderInput{
+			OpenIDConnectProviderArn: aws.String(providerArn),
 		}
-		return err
+
+		_, err := s.Client.DeleteOpenIDConnectProvider(i)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case iam.ErrCodeNoSuchEntityException:
+					s.scope.Info("OIDC provider no longer exists, skipping deletion")
+					continue
+				}
+			}
+			return err
+		}
+		s.scope.Info("Deleted OIDC provider")
 	}
-	s.scope.Info("Deleted OIDC provider")
 
 	return nil
 }
