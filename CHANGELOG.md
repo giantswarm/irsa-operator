099b011563010487f68483be268e2ba44ddb8510
Christian Bianchi <whites11@gmail.com>
2023-05-22 17:10:41 +0200

    Enable public access for S3 in china.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 - Enable s3 bucket ACL.
+- Enable public access for S3 in china.
 
 ## [0.14.1] - 2023-05-18
 
--- pkg/irsa/legacy/legacy.go
+++ pkg/irsa/legacy/legacy.go
@@ -298,14 +298,12 @@ func (s *Service) Reconcile(ctx context.Context) error {
 	}
 	// restrict access only for non-China region and v18.x.x release or higher
 	if (!key.IsChina(s.Scope.Region()) && key.IsV18Release(s.Scope.Release())) || (s.Scope.MigrationNeeded() && !key.IsChina(s.Scope.Region())) {
-		s.Scope.Logger().Error(err, "=============================== blocking public access")
 		err = s.S3.BlockPublicAccess(s.Scope.BucketName())
 		if err != nil {
 			s.Scope.Logger().Error(err, "failed to block public access")
 			return err
 		}
 	} else {
-		s.Scope.Logger().Error(err, "=============================== allowing public access")
 		err = s.S3.AllowPublicAccess(s.Scope.BucketName())
 		if err != nil {
 			s.Scope.Logger().Error(err, "failed to allow public access")
