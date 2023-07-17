d2c0ada8c29f9a9df2beb3c9959fce4d853a632f
taylorbot <dev@giantswarm.io>
2023-07-17 07:23:46 +0000

    Release v0.17.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "irsa-operator"
 	source      = "https://github.com/giantswarm/irsa-operator"
-	version     = "0.16.1-dev"
+	version     = "0.17.0"
 )
 
 func Description() string {
