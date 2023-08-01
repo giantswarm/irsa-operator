5257be408f426020b968e6dcf19050c3cdadd2bb
calvix <vaclav@giantswarm.io>
2023-08-01 16:57:59 +0200

    add-eks-support2

--- pkg/aws/services/eks/eks.go
+++ pkg/aws/services/eks/eks.go
@@ -11,12 +11,9 @@ func (s *Service) GetEKSOpenIDConnectProviderURL(clusterName string) (string, er
 	i := &eks.DescribeClusterInput{
 		Name: aws.String(clusterName),
 	}
-
 	cluster, err := s.Client.DescribeCluster(i)
-
 	if err != nil {
 		return "", microerror.Mask(err)
 	}
-
 	return *cluster.Cluster.Identity.Oidc.Issuer, nil
 }
