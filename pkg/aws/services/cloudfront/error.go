09526ad171d40e87c8f553b937f9de6cd9bda20d
Jose Armesto <github@armesto.net>
2023-08-02 10:25:53 +0200

    Avoid blocking the reconciliation loop when deleting (#147)
    * Avoid blocking the reconciliation loop when deleting
    
    * On legacy we don't pass baseDomain

--- pkg/aws/services/cloudfront/error.go
+++ pkg/aws/services/cloudfront/error.go
@@ -1,7 +1,23 @@
 package cloudfront
 
-import "github.com/giantswarm/microerror"
+import (
+	"reflect"
+
+	"github.com/giantswarm/microerror"
+)
 
 var invalidOriginAccessIdentity = &microerror.Error{
 	Kind: "invalidOriginAccessIdentity",
+}
+
+type DistributionNotDisabledError struct {
+	error
+}
+
+func (e *DistributionNotDisabledError) Error() string {
+	return "dns record type is not supported"
+}
+
+func (e *DistributionNotDisabledError) Is(target error) bool {
+	return reflect.TypeOf(target) == reflect.TypeOf(e)
 }
